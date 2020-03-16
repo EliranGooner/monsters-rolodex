0c768b4954ccf29e860e1c49171d1e649972630c
Eliran Zeitouni <eliran.zeitouni@gmail.com>
2020-03-16 09:46:05 +0200

    Updates

---
+++ precache-manifest.f10689f053d2bfddfab12d6215c16454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7350448920a0742cc205c1754b7f46f2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9cb4c09f1784f3380106",
+    "url": "/monsters-rolodex/static/css/main.acba9abe.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb4c09f1784f3380106",
+    "url": "/monsters-rolodex/static/js/main.a93ece40.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
